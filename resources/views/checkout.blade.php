8cf4090121e27bd57a9a4c2469e1d914f9d5df93
Manoel Wilker Alves da Silva <wilker.silva@codevasf.gov.br>
2022-02-18 09:43:04 -0300

    checkout pagseguro

--- resources/views/checkout.blade.php
+++ resources/views/checkout.blade.php
@@ -19,6 +19,7 @@
                 <div class="col-md-12 form-group">
                         <label for="">Número do Cartão de Crédito <span class="brand"></span></label>
                         <input type="text" class="form-control" name="card_number">
+                        <input type="hidden" name="card_brand">
                 </div>
             </div>
             
@@ -42,7 +43,7 @@
                 <div class="col-md-12 installments form-group"></div>
             </div>
 
-            <button class="btn btn-success btn-lg">Efetuar Pagamento</button>
+            <button class="btn btn-success btn-lg processCheckout">Efetuar Pagamento</button>
 
         </form>
     </div>
@@ -54,7 +55,7 @@
 
 @section('scripts')
     <script src="https://stc.sandbox.pagseguro.uol.com.br/pagseguro/api/v2/checkout/pagseguro.directpayment.js"></script>
-
+    <script src="{{ asset('assets/js/jquery.ajax.js')}}"></script>
     <script>
         const sessionId = '{{ session()->get('pagseguro_session_code')}}';
         PagSeguroDirectPayment.setSessionId(sessionId); 
@@ -72,7 +73,7 @@
                     success: function(res){
                         let imgFlag = `<img src="https://stc.pagseguro.uol.com.br/public/img/payment-methods-flags/68x30/${res.brand.name}.png">`
                         spanBrand.innerHTML = imgFlag;
-
+                        document.querySelector('input[name=card_brand]').value = res.brand.name;
                         getInstallments(40, res.brand.name);
                     },
                     error: function(err){
@@ -84,6 +85,45 @@
                 });
             }
         });
+
+        let submitButton = document.querySelector('button.processCheckout');
+
+        submitButton.addEventListener('click', function(event){
+
+            event.preventDefault();
+            
+
+            PagSeguroDirectPayment.createCardToken({
+                cardNumber:         document.querySelector('input[name=card_number]').value,
+                brand:              document.querySelector('input[name=card_brand]').value,
+                cvv:                document.querySelector('input[name=card_cvv]').value,
+                expirationMonth:    document.querySelector('input[name=card_month]').value,
+                expirationYear:     document.querySelector('input[name=card_year]').value,
+                
+                success: function(res){
+                     processPayment(res.card.token);
+                }
+            });
+        });
+
+        function processPayment(token)
+        {
+            let data = {
+                token: token,
+                hash: PagSeguroDirectPayment.getSenderHash(),
+                installment: document.querySelector('select_installments').value
+            };
+
+            $.ajax({
+                type: 'POST',
+                url: '';
+                data: data,
+                dataType: 'json',
+                success: function(res){
+                    console.log(res);
+                }
+            });
+        }
 
         function getInstallments(amount, brand){
             PagSeguroDirectPayment.getInstallments({
@@ -107,7 +147,7 @@
 
         function drawSelectInstallments(installments) {
             let select = '<label>Opções de Parcelamento:</label>';
-            select += '<select class="form-control">';
+            select += '<select class="form-control select_installments">';
             for(let l of installments) {
                 select += `<option value="${l.quantity}|${l.installmentAmount}">${l.quantity}x de ${l.installmentAmount} - Total fica ${l.totalAmount}</option>`;
 		    }
